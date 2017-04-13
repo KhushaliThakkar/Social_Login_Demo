51a2e73002badc9ff90855dc51f6d3ed9cbd118d
KhushaliThakkar <khushali.thakkar@bacancytechnology.com>
2017-04-13 18:06:01 +0530

    Revert "Done"
    This reverts commit e04ba87ffa548e58da814e4e26d19c4c094b2d70.

--- LinkedIn-Auth/config/routes.rb
+++
@@ -1,6 +0,0 @@
-Rails.application.routes.draw do
-  devise_for :users, :controllers => { :omniauth_callbacks => "omniauth_callbacks" }
-  root 'home#index'
-  get 'home/index'
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-end
